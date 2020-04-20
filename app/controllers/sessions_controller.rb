7aa890aadc7a1acd42ef653a91ac2cc09cc92c92
tirnavali <tran.ce.co@gmail.com>
2020-04-20 19:24:46 +0300

    Authentication özelliği eklendi ve tamamlandı.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,20 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+  	user = User.authenticate(params[:email], params[:password])
+  	if user
+  		session[:user_id] = user.id
+  		redirect_to root_url, :notice => "Giriş yapıldı!"
+  	else
+  		flash.now.alert = "Yanlış email yada şifre girdiniz!"
+  		render "new"
+  	end
+  end
+
+  def destroy
+  	session[:user_id] = nil
+  	redirect_to new_session_path, :notice => "Çıkış yapıldı!"
+  end
 end
