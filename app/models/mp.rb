fe052c939f10b2d4f5142c8fe03c81b1ec8d2a25
tirnavali <tran.ce.co@gmail.com>
2020-04-26 00:56:13 +0300

    Mp validasyonları yapıldı.

--- app/models/mp.rb
+++ app/models/mp.rb
@@ -1,11 +1,21 @@
+# frozen_string_literal: true
+
 class Mp < ApplicationRecord
-	has_many :elections
-	has_many :periods, :through => :elections
-	has_many :parties, :through => :elections
+  validates :name, :surname, :gender, :birthdate, presence: true
+  validate :olum_tarihi_dogum_tarihinden_kucuk_olamaz
+  has_many :elections
+  has_many :periods, through: :elections
+  has_many :parties, through: :elections
 
+  def olum_tarihi_dogum_tarihinden_kucuk_olamaz
+    unless deaddate.nil?
+      if deaddate < birthdate || deaddate == birthdate
+        errors.add(:deaddate, 'Doğum tarihinden küçük ve eşit olamaz!')
+      end
+    end
+  end
 
-	def full_name
-		"#{name}, #{name2} #{surname}"
-	end
-
+  def full_name
+    "#{name}, #{name2} #{surname}"
+  end
 end
